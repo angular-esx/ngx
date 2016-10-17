f4d5a1e50388ede89c17b10b2d2336eed652d158
Minh Van <minh.van1789@gmail.com>
2016-10-17 13:49:18 +0700

    Init modules/index.js

---
+++ projects/ngx-framework/modules/index.js
@@ -0,0 +1,36 @@
+export {
+  ngxCardModule,
+  ngxCardHeaderComponent,
+  ngxCardTitleComponent,
+  ngxCardSubtitleComponent,
+  ngxCardTextComponent,
+  ngxCardImageComponent,
+  ngxCardContentComponent,
+  ngxCardActionsComponent,
+  ngxCardComponent
+} from './card';
+
+export {
+  ngxGridModule,
+  ngxGridRowDirective,
+  ngxGridColumnDirective,
+  ngxGridComponent
+} from './grid';
+
+export {
+  ngxJumbotronModule,
+  ngxJumbotronComponent
+} from './jumbotron';
+
+export {
+  ngxLabelModule,
+  ngxLabelComponent
+} from './label';
+
+export {
+  ngxNavbarModule,
+  ngxNavbarBrandDirective,
+  ngxNavbarDividerDirective,
+  ngxNavbarItemDirective,
+  ngxNavbarComponent
+} from './navbar';
