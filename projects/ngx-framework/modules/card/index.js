92705e36726d70f43907de7c9bfa0fe0599cf117
Minh Van <minh.van1789@gmail.com>
2016-10-17 13:49:04 +0700

    Corrected modules/card/index.js

--- projects/ngx-framework/modules/card/index.js
+++ projects/ngx-framework/modules/card/index.js
@@ -1,4 +1,10 @@
 ﻿export { ngxCardModule } from './card.module';
 
 export { ngxCardHeaderComponent } from './card-header.component';
+export { ngxCardTitleComponent } from './card-title.component';
+export { ngxCardSubtitleComponent } from './card-subtitle.component';
+export { ngxCardTextComponent } from './card-text.component';
+export { ngxCardImageComponent } from './card-image.component';
+export { ngxCardContentComponent } from './card-content.component';
+export { ngxCardActionsComponent } from './card-actions.component';
 export { ngxCardComponent } from './card.component';
